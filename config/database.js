4590c2bbac5a496533d2e1f61b376b5a16778da5
Ramakrishnan Iyer <iyerramakrishnan5@gmail.com>
2018-02-23 08:24:55 +0530

    Leave Tracker v1.0

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
+//const MONGOLAB_URI = 'mongodb://rama:11@Gooner@ds247078.mlab.com:47078/users'
+const url = process.env.MONGOLAB_URI;
 module.exports = {
-    database:'mongodb://localhost:27017/users',
+    database: url ||'mongodb://localhost:27017/users',
     secret:'11@Gooner'
 }
